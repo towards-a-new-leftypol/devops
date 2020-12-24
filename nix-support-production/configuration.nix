59b9591774b22875df599501ec9ec52cbc33727b
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2020-12-24 16:59:45 -0500

    copy of the .nix files currently serving the site

---
+++ nix-support-production/configuration.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ./users.nix
+    ./lainchan.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    wget
+    gitAndTools.gitFull
+    inetutils
+  ];
+
+  boot.isContainer = true;
+  environment.noXlibs = true;
+  
+  services.openssh.enable = true;
+  services.openssh.startWhenNeeded = false;
+  services.openssh.passwordAuthentication = false;
+  systemd.services.sshd.wantedBy = lib.mkOverride 40 [ "multi-user.target" ];
+
+  networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+  networking.hostName = "LPDev";
+  networking.nameservers = [ "213.186.33.99" ];
+}
