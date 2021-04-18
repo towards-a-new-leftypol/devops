be542b0d7dd2597db6ebd4b0c6676097ad268fae
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2021-04-18 18:40:13 -0400

    Port over init script changes to production

--- nix-support-production/configuration.nix
+++ nix-support-production/configuration.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    <nixpkgs/nixos/modules/virtualisation/lxc-container.nix>
     ./users.nix
     ./nginx.nix
     ./lainchan.nix
@@ -28,4 +29,10 @@
   networking.firewall.allowedTCPPorts = [ 22 8080 443 ];
   networking.hostName = "LPProd";
   networking.nameservers = [ "213.186.33.99" ];
+
+  # Install new init script
+  system.activationScripts.installInitScript = lib.mkForce ''
+    mkdir -p /sbin
+    ln -fs $systemConfig/init /sbin/init
+  '';
 }
