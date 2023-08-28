55fc6e7e3a1fb9331b2193e434210333f40e6b23
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2023-08-28 19:57:57 +0000

    Postgresql services improvements (works)
    - don't keep jwt secrets in the repo

--- nix-support-dev/postgresql.nix
+++ nix-support-dev/postgresql.nix
@@ -3,41 +3,43 @@
 let
   spgist = pkgs.callPackage ./pg_spgist_nix/default.nix {};
 
+  initScript = builtins.toFile "init-script.sql" (
+    (builtins.readFile "${spgist}/init.sql") + ''
+
+    CREATE EXTENSION pg_trgm;
+  '');
 in
 
 {
-    services.postgresql = {
-        enable = true;
-        enableTCPIP = true;
-        package = pkgs.postgresql;
-        authentication = ''
-            host    all     all     localhost       password
-        '';
-        settings = {
-          shared_buffers = "2GB";
-          work_mem = "16MB";
-        };
+  services.postgresql = {
+      enable = true;
+      #enableTCPIP = true;
+      package = pkgs.postgresql;
+      authentication = ''
+          host    all     all     localhost       password
+      '';
+      settings = {
+        shared_buffers = "2GB";
+        work_mem = "16MB";
+      };
 
-        ensureDatabases = [ "leftypol_test" ];
+      ensureDatabases = [ "leftypol_test" ];
 
-        ensureUsers = [
-          {
-            name = "admin";
-            ensurePermissions = {
-              "DATABASE \"leftypol_test\"" = "ALL PRIVILEGES";
-              "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
-            };
-          }
-        ];
+      ensureUsers = [
+        {
+          name = "admin";
+          ensurePermissions = {
+            "DATABASE \"leftypol_test\"" = "ALL PRIVILEGES";
+            "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
+          };
+        }
+      ];
 
-        initialScript = (builtins.readFile "${spgist}/init.sql") + ''
+      initialScript = initScript;
 
-          CREATE EXTENSION pg_trgm; -- only superuser can
-        '';
-
-        extraPlugins = [
-          spgist
-        ];
-    };
+      extraPlugins = [
+        spgist
+      ];
+  };
 }
 
