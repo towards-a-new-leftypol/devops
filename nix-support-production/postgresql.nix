f84aba80e64c9eaf84afc2a17552e2924113931a
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2023-09-21 15:25:36 -0400

    Add nix files from dev (for spamnoticer)

---
+++ nix-support-production/postgresql.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, lib, ... }:
+
+let
+  spgist = pkgs.callPackage ./pg_spgist_nix/default.nix {};
+
+  # initScript = builtins.toFile "init-script.sql" (
+  #   (builtins.readFile "${spgist}/init.sql") + ''
+
+  #   CREATE EXTENSION pg_trgm;
+  # '');
+in
+
+{
+  services.postgresql = {
+      enable = true;
+      enableTCPIP = true;
+      package = pkgs.postgresql;
+      authentication = ''
+          host    all     all     localhost            md5
+          host    all     all     10.207.38.0/24       md5
+          host    all     all     10.4.0.0/24          md5
+      '';
+      settings = {
+        shared_buffers = "2GB";
+        work_mem = "16MB";
+      };
+
+      #ensureDatabases = [ "leftypol_test" ];
+
+      ensureUsers = [
+        {
+          name = "admin";
+          ensureClauses = {
+            createrole = true;
+            createdb = true;
+          };
+        }
+      ];
+
+      # initialScript = initScript;
+
+      extraPlugins = [
+        spgist
+      ];
+  };
+}
+
