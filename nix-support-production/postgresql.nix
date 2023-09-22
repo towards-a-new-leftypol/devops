df999e202aba06e3925d538221b3b7708a836f48
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2023-09-22 13:35:35 -0400

    Remove all except localhost from prod pg_hba

--- nix-support-production/postgresql.nix
+++ nix-support-production/postgresql.nix
@@ -17,8 +17,6 @@ in
       package = pkgs.postgresql;
       authentication = ''
           host    all     all     localhost            md5
-          host    all     all     10.207.38.0/24       md5
-          host    all     all     10.4.0.0/24          md5
       '';
       settings = {
         shared_buffers = "2GB";
